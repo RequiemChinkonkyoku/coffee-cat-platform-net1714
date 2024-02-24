417529b981acf26689c371f9f64ab6454c974b3a
RequiemRekuiemu <phuongdo0207@gmail.com>
2024-02-24 17:14:08 +0700

    added staff folder

---
+++ CoffeeCatPlatform/Pages/StaffPages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CoffeeCatPlatform.Pages.StaffPages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
