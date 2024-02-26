32f1cf079811d8dd6cbbf10dca632f8d58b2c60a
TNKLBAH <putinkhanh112@gmail.com>
2024-02-26 22:27:45 +0700

    temp commit

---
+++ CoffeeCatPlatform/Pages/MoMoPages/MoMoInfo.cshtml.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CoffeeCatPlatform.Pages.MoMoPages
+{
+    public class MoMoInfo : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+
+        public async Task OnPost()
+        {
+
+        }
+    }
+}
