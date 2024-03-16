019cf1b6befce56ec1e6e8f1980eadd24ef8827b
TienTran23 <trannhuttienvn23@gmail.com>
2024-03-16 12:31:47 +0700

    add search function

--- CoffeeCatPlatform/Pages/MenuPages/Menu.cshtml.cs
+++ CoffeeCatPlatform/Pages/MenuPages/Menu.cshtml.cs
@@ -11,51 +11,98 @@ using Repositories;
 using Repositories.Impl;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.Extensions.Configuration;
+using System.ComponentModel.DataAnnotations;
 
 namespace CoffeeCatPlatform.Pages.MenuPages
 {
-    public class MenuModel : PageModel
-    {
-        private readonly IRepositoryBase<Product> _productRepo;
+	public class MenuModel : PageModel
+	{
+		private readonly IRepositoryBase<Product> _productRepo;
 
-        public bool result = false;
+		public bool result = false;
 
-        public MenuModel()
-        {
-            _productRepo = new ProductRepository();
+		public MenuModel()
+		{
+			_productRepo = new ProductRepository();
 
-        }
-        [BindProperty(SupportsGet = true)]
+		}
+		[BindProperty(SupportsGet = true)]
 
-        public int CurrentPage { get; set; } = 1;
-        public int TotalItems { get; set; }
-        public int ItemsPerPage { get; set; } = 4;
+		public int CurrentPage { get; set; } = 1;
+		public int TotalItems { get; set; }
+		public int ItemsPerPage { get; set; } = 4;
 
+		[BindProperty(SupportsGet = true)]
+		public string SearchQuery { get; set; }
 
-        public IList<Product> Products { get; set; } = default!;
+		public IList<Product> Products { get; set; } = default!;
 
-        public int TotalPages => (int)Math.Ceiling((double)TotalItems / ItemsPerPage);
+		public int TotalPages => (int)Math.Ceiling((double)TotalItems / ItemsPerPage);
 
-        public IActionResult OnGet(string? currentPage)
-        {
-            if (int.TryParse(currentPage, out int temp))
-            {
-                CurrentPage = temp;
-            }
-            else
-            {
-                CurrentPage = 1;
-            }
+		public IActionResult OnGet(string? currentPage, string? searchQuery)
+		{
+			SearchQuery = searchQuery;
 
-            var allProducts = _productRepo.GetPaginated(CurrentPage, ItemsPerPage);
+			if (int.TryParse(currentPage, out int temp))
+			{
+				CurrentPage = temp;
+			}
+			else
+			{
+				CurrentPage = 1;
+			}
 
-            TotalItems = _productRepo.GetAll().Count;
+			if (!string.IsNullOrEmpty(searchQuery))
+			{
+				Products = _productRepo.GetAll()
+					.Where(p => p.Name.IndexOf(searchQuery, StringComparison.OrdinalIgnoreCase) >= 0)
+					.ToList();
 
-            Products = allProducts;
+				TotalItems = Products.Count;
 
-            result = Products.Count > 0;
+				Products = Products.Skip((CurrentPage - 1) * ItemsPerPage)
+								   .Take(ItemsPerPage)
+								   .ToList();
+			}
+			else
+			{
+				var allProducts = _productRepo.GetPaginated(CurrentPage, ItemsPerPage);
+				TotalItems = _productRepo.GetAll().Count;
+				Products = allProducts;
+			}
 
-            return Page();
-        }
-    }
+			result = Products.Count > 0;
+
+			return Page();
+		}
+
+		public IActionResult OnPost(string? currentPage, string? searchQuery)
+		{
+
+			 if (!string.IsNullOrEmpty(searchQuery))
+			{
+				Products = _productRepo.GetAll()
+					.Where(p => p.Name.IndexOf(searchQuery, StringComparison.OrdinalIgnoreCase) >= 0)
+					.ToList();
+
+				TotalItems = Products.Count;
+
+				Products = Products.Skip((CurrentPage - 1) * ItemsPerPage)
+								   .Take(ItemsPerPage)
+								   .ToList();
+			}
+			else
+			{
+				var allProducts = _productRepo.GetPaginated(CurrentPage, ItemsPerPage);
+				TotalItems = _productRepo.GetAll().Count;
+				Products = allProducts;
+			}
+
+			result = Products.Count > 0;
+
+			
+
+			return Page();
+		}
+	}
 }
