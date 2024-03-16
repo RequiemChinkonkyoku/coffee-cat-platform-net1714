1924b9661495cffbdd8669390316e544afd799bb
MinhNg666 <230303milo@gmail.com>
2024-03-16 16:52:58 +0700

    fix View My Account

---
+++ CoffeeCatPlatform/Pages/AreaManagement/ViewArea.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CoffeeCatPlatform.Pages.AreaManagement
+{
+    public class ViewAreaModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
