c9c20bab5f455e3fc3a236eac4ac4683349de581
RequiemRekuiemu <phuongdo0207@gmail.com>
2024-03-23 00:19:03 +0700

    implemented fe for table management

--- CoffeeCatPlatform/Pages/TableManagement/ViewTable.cshtml.cs
+++ CoffeeCatPlatform/Pages/TableManagement/ViewTable.cshtml.cs
@@ -10,20 +10,26 @@ namespace CoffeeCatPlatform.Pages.TableManagement
     public class ViewTableModel : PageModel
     {
         private readonly IRepositoryBase<Table> _tableRepo;
+        private readonly IRepositoryBase<Area> _areaRepo;
         public bool result = false;
 
-        public ViewTableModel() 
+        public ViewTableModel()
         {
             _tableRepo = new TableRepository();
+            _areaRepo = new AreaRepository();
         }
 
         public IList<Table> Tables { get; set; } = default!;
 
         public void OnGet()
         {
-            if (!_tableRepo.GetAll().IsNullOrEmpty()) 
+            if (!_tableRepo.GetAll().IsNullOrEmpty())
             {
                 Tables = _tableRepo.GetAll();
+                foreach (var table in Tables)
+                {
+                    table.Area = _areaRepo.GetAll().FirstOrDefault(x => x.AreaId == table.AreaId);
+                }
                 result = true;
             }
         }
