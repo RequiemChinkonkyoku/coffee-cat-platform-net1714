9daffbe3eb003ec9e692d98893130c686715e5eb
RequiemRekuiemu <phuongdo0207@gmail.com>
2024-03-23 18:30:01 +0700

    final push - bye bye

--- CoffeeCatPlatform/Pages/CatManagement/CreateCat.cshtml.cs
+++ CoffeeCatPlatform/Pages/CatManagement/CreateCat.cshtml.cs
@@ -59,6 +59,7 @@ namespace CoffeeCatPlatform.Pages.CatManagement
             Cat.ShopId = 1;
             _catRepository.Add(Cat);
             AreaCat.CatId = Cat.CatId;
+            AreaCat.AreaId = 1;
             _areacatRepository.Add(AreaCat);
 
             TempData["SuccessMessage"] = "Cat created successfully.";
